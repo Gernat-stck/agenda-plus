f5bb251813cdd9cc6910e4e9cdb7d60328af0312
Gernat <nagerman26@gmail.com>
2025-02-17 22:08:55 -0600

    Agregar componente RecommendedSections para mostrar enlaces a gestionar citas, ver clientes y servicios en el panel de control

---
+++ src/components/dashboard/RecommendedSections.tsx
@@ -0,0 +1,50 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import { Button } from "../ui/button"
+import { BriefcaseBusiness, Calendar, Users } from "lucide-react"
+import Link from "next/link"
+
+export default function RecomendedSection() {
+    return (
+        <>
+            <Card className="md:col-span-3">
+                <CardHeader>
+                    <CardTitle>Secciones Recomendadas</CardTitle>
+                </CardHeader>
+                <CardContent>
+                    <div className="grid gap-4 md:grid-cols-3">
+                        <Link
+                            href={"/citas"}
+                            passHref
+                            className="h-20 justify-start"
+                        >
+                            <Button variant="outline" className="h-full w-full">
+                                <Calendar className="mr-2" />
+                                Gestionar Citas
+                            </Button>
+                        </Link>
+                        <Link
+                            href={"/clientes"}
+                            passHref
+                            className="h-20 justify-start"
+                        >
+                            <Button variant="outline" className="h-full w-full">
+                                <Users className="mr-2" />
+                                Ver Clientes
+                            </Button>
+                        </Link>
+                        <Link
+                            href={"/servicios"}
+                            passHref
+                            className="h-20 justify-start"
+                        >
+                            <Button variant="outline" className="h-full w-full">
+                                <BriefcaseBusiness className="mr-2" />
+                                Ver Servicios
+                            </Button>
+                        </Link>
+                    </div>
+                </CardContent>
+            </Card>
+        </>
+    )
+}
