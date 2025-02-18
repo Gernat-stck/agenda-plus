6c777268674dbbe39580a635b888696f39534d31
Gernat <nagerman26@gmail.com>
2025-02-17 22:08:01 -0600

    Reemplazar contenido de la página principal con el componente Dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,5 @@
+import Dashboard from "./dashboard/page";
+
 export default function Home() {
-  console.log("Hola desde El children");
-  return (
-    <div>
-      <h1 className="text-4xl font-bold text-center text-gray-800">
-        Hola Mundo
-      </h1>
-    </div>
-  );
+  return <Dashboard />;
 }
