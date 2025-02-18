02496c92d872a5462a77ac8a9746e0d0e667a492
Gernat <nagerman26@gmail.com>
2025-02-17 22:07:56 -0600

    Actualizar diseño del layout para mejorar la navegación y agregar soporte para scroll en el contenido

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,38 +1,40 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import Navbar from "@/components/Navbar";
-import "./globals.css";
+import type React from "react"
+import type { Metadata } from "next"
+import { Geist, Azeret_Mono as Geist_Mono } from "next/font/google"
+import Navbar from "@/components/Navbar"
+import "@/app/css/globals.css"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
-});
+})
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
-});
+})
 
 export const metadata: Metadata = {
   title: "Agenda Plus +",
   description: "Registra tus citas, clientes y servicios de manera sencilla.",
-};
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <div className="min-h-screen bg-gray-50 flex">
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <div className="flex h-screen overflow-hidden bg-gray-50">
           <Navbar />
-          <div className="ml-20 flex-1 p-4">{children}</div>
+          <div className="flex-1 overflow-y-scroll scrollbar">
+            <main className="h-full">{children}</main>
+          </div>
         </div>
       </body>
     </html>
-  );
+  )
 }
+
