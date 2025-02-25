e4c717e3a889159c9e0618eba4885ae793f15bdb
Gernat <nagerman26@gmail.com>
2025-02-24 19:47:40 -0600

    Eliminar componentes de cliente y formulario de cliente

--- src/app/providers.tsx
+++
@@ -1,9 +0,0 @@
-// app/providers.tsx
-"use client";
-
-import { ReactNode } from "react";
-import { CitaProvider } from "@/app/contexts/CitaContext";
-
-export function Providers({ children }: { children: ReactNode }) {
-    return <CitaProvider>{children}</CitaProvider>;
-}
