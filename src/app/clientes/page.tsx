a4d0a22e57958e51d3019e6ba94cc638bb5a36b9
Gernat <nagerman26@gmail.com>
2025-02-24 19:48:05 -0600

    Agregar componente ListaClientes a la vista de citas

--- src/app/clientes/page.tsx
+++ src/app/clientes/page.tsx
@@ -1,25 +1,28 @@
 "use client"
 import Header from "@/app/components/Header";
 import React from "react";
-//TODO: Implementar la vista de citas
-return (
-  <div className="h-screen overflow-hidden p-2 ">
-    <div className="max-w-7xl mx-auto p-6">
-      {/*<!-- Header -->*/}
-      <Header
-        title="Citas"
-        subtitle="Agenda+"
-        userName="Usuario"
-        userRole="Emprendedor"
-      />
-      {/*<!-- Main Content -->*/}
-      <main className="flex-grow flex items-stretch">
-        <div className="w-full flex items-center justify-center">
-          <div className="w-full -mt-6 mr-8">
+import ListaClientes from "@/app/components/clients/ListaClientes";
+
+export default function CitasPage() {
+  return (
+    <div className="h-screen overflow-hidden p-2 ">
+      <div className="max-w-7xl mx-auto p-6">
+        {/*<!-- Header -->*/}
+        <Header
+          title="Citas"
+          subtitle="Agenda+"
+          userName="Usuario"
+          userRole="Emprendedor"
+        />
+        {/*<!-- Main Content -->*/}
+        <main className="flex-grow flex items-stretch">
+          <div className="w-full flex items-center justify-center">
+            <div className="w-full -mt-6 mr-8">
+              <ListaClientes />
+            </div>
           </div>
-        </div>
-      </main>
+        </main>
+      </div>
     </div>
-  </div>
-);
+  );
 }
