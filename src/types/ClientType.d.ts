31ac5a823d3db525653f42c51932f1b73dc7de3e
Gernat <nagerman26@gmail.com>
2025-03-02 20:35:50 -0600

    Se migrara el proyecto a Laravel 12 + Vite

--- src/types/ClientType.d.ts
+++
@@ -1,9 +0,0 @@
-export interface Cliente {
-  id: string;
-  nombre: string;
-  contacto: number;
-  correo: string;
-  fechaCita: string;
-  estadoCita: string;
-  paymentMethod?: "efectivo" | "tarjeta";
-}
