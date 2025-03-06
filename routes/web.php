4a46f937978c8b80e8e07551b2df49d722dac347
Gernat <nagerman26@gmail.com>
2025-03-05 21:22:53 -0600

    Agregar ruta para el calendario en el archivo de rutas

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,9 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
+    Route::get('calendar', function () {
+        return Inertia::render('calendar');
+    })->name('calendar');
 });
 
 require __DIR__.'/settings.php';
