dce321cac6957591fc591ee86c1f7a2c5c52669e
Gernat <nagerman26@gmail.com>
2025-03-17 21:28:09 -0600

    Agregar configuración de Twilio en services.php para integración con WhatsApp

--- config/services.php
+++ config/services.php
@@ -34,5 +34,11 @@ return [
             'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
         ],
     ],
+    'twilio' => [
+        'account_sid' => env('TWILIO_ACCOUNT_SID', ''),
+        'auth_token' => env('TWILIO_AUTH_TOKEN', ''),
+        'phone_number' => env('TWILIO_PHONE_NUMBER', ''),
+        'from' => env('TWILIO_WHATSAPP_FROM', ''),
+    ],
 
 ];
