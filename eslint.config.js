c935ac5f26892da184e3443c3e4ebad68e29a6f3
Gernat <nagerman26@gmail.com>
2025-03-24 17:14:53 -0600

    Actualizar configuraciones de ESLint y Prettier, incluyendo nuevas versiones de dependencias y ajustes en las reglas

--- eslint.config.js
+++ eslint.config.js
@@ -1,31 +1,26 @@
 /** @type {import('eslint').Linter.Config} */
 export default {
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaVersion: 2021,
-        sourceType: 'module',
-        ecmaFeatures: {
-            jsx: true,
-        },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
     },
-    plugins: ['@typescript-eslint', 'react', 'react-hooks'],
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:react/recommended',
-        'plugin:react-hooks/recommended',
-        'prettier',
-    ],
-    settings: {
-        react: {
-            version: 'detect',
-        },
+  },
+  plugins: ['@typescript-eslint', 'react', 'react-hooks'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react/recommended', 'plugin:react-hooks/recommended', 'prettier'],
+  settings: {
+    react: {
+      version: 'detect',
     },
-    rules: {
-        'react/react-in-jsx-scope': 'off', // Si usas React 17+
-        'react/prop-types': 'off', // Si no usas PropTypes
-        '@typescript-eslint/no-unused-vars': 'warn',
-        '@typescript-eslint/explicit-function-return-type': 'off',
-    },
-    ignorePatterns: ['vendor/', 'node_modules/', 'public/', '*.config.js'],
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off', // Si usas React 17+
+    'react/prop-types': 'off', // Si no usas PropTypes
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+  },
+  ignorePatterns: ['vendor/', 'node_modules/', 'public/', '*.config.js'],
 };
