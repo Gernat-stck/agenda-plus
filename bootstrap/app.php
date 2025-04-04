34499d6afde93a90909172da17716bfdad557632
Gernat <nagerman26@gmail.com>
2025-04-03 20:48:28 -0600

    Eliminar línea en la configuración de rutas en bootstrap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,6 @@ return Application::configure(basePath: dirname(__DIR__))
         web: __DIR__ . '/../routes/web.php',
         commands: __DIR__ . '/../routes/console.php',
         api: __DIR__ . '/../routes/api.php',
-
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
